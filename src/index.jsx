5318245e07db87812c2ed8ae37653151b254afe4
Traphume <traphumedev@gmail.com>
2020-05-22 22:02:14 +0700

    Added login state

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function HelloWorld() {
-  return <h1>Hello, World!</h1>;
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedin: false,
+    };
+  }
+
+  render() {
+    const { isLoggedin } = this.state;
+
+    // Proceed to render application
+    if (isLoggedin) {
+      return <h1>You are logged in!</h1>;
+    }
+
+    // Show login form
+    return (
+      <div>
+        <h1>You are not logged in!</h1>
+        <button
+          type="submit"
+          onClick={() => this.setState({ isLoggedin: true })}
+        >
+          Login
+        </button>
+      </div>
+    );
+  }
 }
 
-ReactDom.render(HelloWorld(), document.getElementById("root"));
+ReactDom.render(<Main />, document.getElementById("root"));
